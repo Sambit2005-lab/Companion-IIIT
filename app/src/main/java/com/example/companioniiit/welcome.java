7a6f98f81d0ab11bdf8c1d3830fe25aa650955f7
Abhishek Bairagi <abhishekbairagi2005@gmail.com>
2024-05-22 22:56:56 +0530

    welcome_backend_created

--- app/src/main/java/com/example/companioniiit/welcome.java
+++ app/src/main/java/com/example/companioniiit/welcome.java
@@ -1,6 +1,9 @@
 package com.example.companioniiit;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,13 +12,31 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class welcome extends AppCompatActivity {
+    Button btn1,btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_welcome);
+        btn1 = findViewById(R.id.signup_btn);
+        btn2 = findViewById(R.id.login_btn);
 
+        btn1.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        Intent intent;
+                                        intent = new Intent(welcome.this, signup.class);
+                                        startActivity(intent);
 
-    }
-}
+                                    }
+                                });
+        btn2.setOnClickListener(new View.OnClickListener(){
+
+                public void onClick(View v) {
+                    Intent intent;
+                    intent = new Intent(welcome.this, login.class);
+                    startActivity(intent);
+
+                }
+        });
+    }}
