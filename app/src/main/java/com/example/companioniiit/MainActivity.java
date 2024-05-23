723ee2e3ee864dfa7552a2fa296bebc3bb5af656
Abhishek Bairagi <abhishekbairagi2005@gmail.com>
2024-05-23 09:58:01 +0530

    Done_signup_And_login

--- app/src/main/java/com/example/companioniiit/MainActivity.java
+++ app/src/main/java/com/example/companioniiit/MainActivity.java
@@ -13,7 +13,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
     }
