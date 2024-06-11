1bfb23378d57f1b50fdfdd74d012849dd42f5f71
Sambit2005-lab <143246577+Sambit2005-lab@users.noreply.github.com>
2024-06-11 12:54:18 +0530

    tab layout functionality created

---
+++ app/src/main/java/com/example/companioniiit/ViewPagerAdapter.java
@@ -0,0 +1,44 @@
+package com.example.companioniiit;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    public ViewPagerAdapter(@NonNull FragmentManager fm) {
+        super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new NoteFragment();
+        } else {
+            return new PyqFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0) {
+            return "Notes";
+        } else {
+            return "Pyq";
+        }
+    }
+}
+
+
+
+
+
+
